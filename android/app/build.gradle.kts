77cf8cba03c3f66925bf3a472ef90ac31d7d99f4
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-08 23:53:31 +0700

    CHG - graddle ndkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.bloc_practicum_assignment"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
