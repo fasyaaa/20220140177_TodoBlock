84c0b0f78487e394cb986077552dc95a18e61ceb
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-09 01:21:35 +0700

    ADD - Setup form key and text controller

--- lib/bloc/todo_page.dart
+++ lib/bloc/todo_page.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TodoPage extends StatefulWidget {
+class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
-  State<TodoPage> createState() => _TodoPageState();
-}
-
-class _TodoPageState extends State<TodoPage> {
-  @override
   Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
-        ),
+        child: Column(children: []),
       ),
     );
   }
 }
-
