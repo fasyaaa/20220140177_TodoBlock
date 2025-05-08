3391276f2b4736021d2ae22776d5f2a7f155ce3f
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-09 01:14:55 +0700

    ADD - Add TodoBloc dengan event handling

--- lib/bloc/todo_bloc_bloc.dart
+++ lib/bloc/todo_bloc_bloc.dart
@@ -6,9 +6,47 @@ part 'todo_bloc_event.dart';
 part 'todo_bloc_state.dart';
 
 class TodoBlocBloc extends Bloc<TodoBlocEvent, TodoBlocState> {
-  TodoBlocBloc() : super(TodoBlocInitial()) {
-    on<TodoBlocEvent>((event, emit) {
-      // TODO: implement event handler
+  TodoBlocBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
+    on<TodoEventAdd>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentState.todos);
+        updateTodos.add(
+          Todo(title: event.title, isComplete: false, date: event.date),
+        );
+        emit(
+          TodoLoaded(
+            todos: updateTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      }
+    });
+
+    on<TodoSelectDate>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        emit(TodoLoaded(todos: currentState.todos, selectedDate: event.date));
+      }
+    });
+    on<TodoEventComplete>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currentState.todos);
+        if (event.index >= 0 && event.index < updatedTodos.length) {
+          updatedTodos[event.index] = Todo(
+            title: updatedTodos[event.index].title,
+            isComplete: !updatedTodos[event.index].isComplete,
+            date: updatedTodos[event.index].date,
+          );
+          emit(
+            TodoLoaded(
+              todos: updatedTodos,
+              selectedDate: currentState.selectedDate,
+            ),
+          );
+        }
+      }
     });
   }
 }
