698341c43b7dbdbcf4517ad1d367ac92aa5519fd
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-09 00:36:24 +0700

    ADD - TodoEventAdd class

--- lib/bloc/todo_bloc_event.dart
+++ lib/bloc/todo_bloc_event.dart
@@ -2,3 +2,11 @@ part of 'todo_bloc_bloc.dart';
 
 @immutable
 sealed class TodoBlocEvent {}
+
+final class TodoEventAdd extends TodoBlocEvent {
+  final String title;
+  final DateTime date;
+  TodoEventAdd({required this.date, required this.title});
+}
+
+
